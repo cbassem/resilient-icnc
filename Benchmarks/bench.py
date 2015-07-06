0765fd24d4a039ec8a89a9998089ac648fb33804
cbassem <cbassem@vub.ac.be>
2015-07-06 12:20:40 +0200

    benchmark stuff

---
+++ Benchmarks/bench.py
@@ -0,0 +1,39 @@
+import subprocess
+import sys
+import os
+
+def persist(name, nodes, iters, results):
+	filename = name + '_' + nodes + '_' + str(iters)
+	try:
+		os.remove(filename)
+	except OSError:
+		pass
+	with open(filename, 'w+') as file:
+		for i in results:		
+			file.write(i+'\n')
+
+def process(path, nodes, iters):
+	cmd = ''.join(['env CNC_LOAD_BALANCER=STEALING env DIST_CNC=MPI /usr/bin/mpiexec.hydra -n ', nodes, ' ', path])
+	p = subprocess.Popen([cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	out, err = p.communicate()
+        out = out.split()	
+	time = out[out.index("RUNTIME") + 1]
+	return time
+
+#Usage: bench nodes iters benchname /path/to/program
+def main(argv=sys.argv):
+	nodes = argv[1]
+	iters = int(argv[2])
+	name = argv[3]
+	path = argv[4]
+
+	results = []
+	for i in range(0, iters):
+		r = process(path, nodes, iters)
+		results = results + [r]
+	persist(name, nodes, iters, results)
+
+if __name__ == '__main__':
+    main()
+
+
--- samples/fib/fib_mpi_tuners/fib.cpp
+++ samples/fib/fib_mpi_tuners/fib.cpp
@@ -95,10 +95,19 @@ void handler(int sig) {
   backtrace_symbols_fd(array, size, STDERR_FILENO);
   exit(1);
 }
+typedef unsigned long long timestamp_t;
+static timestamp_t get_timestamp ()
+{
+  struct timeval now;
+  gettimeofday (&now, NULL);
+  return  now.tv_usec + (timestamp_t)now.tv_sec * 1000000;
+}
 
 int main( int argc, char* argv[] )
 {
-	signal(SIGSEGV, handler);
+    timestamp_t t0 = get_timestamp();
+
+	//signal(SIGSEGV, handler);
     CnC::dist_cnc_init<fib_context> _dinit;
     int n = 42;
     // eval command line args
@@ -108,26 +117,31 @@ int main( int argc, char* argv[] )
 
     // create context
     fib_context ctxt;
-    CnC::debug::collect_scheduler_statistics( ctxt );
+    //CnC::debug::collect_scheduler_statistics( ctxt );
 
 
     // put tags to initiate evaluation
     for( int i = 0; i <= n; ++i ) ctxt.m_tags.put( i );
-    std::cout<<"start waiting" <<std::endl;
+    //std::cout<<"start waiting" <<std::endl;
     // wait for completion
     ctxt.wait();
-    std::cout<<"done waiting" <<std::endl;
+    //std::cout<<"done waiting" <<std::endl;
     // get result
     fib_type res2;
     ctxt.m_fibs.get( n, res2 );
 
     // print result
-    std::cout << "fib (" << n << "): " << res2 << std::endl;
+    //std::cout << "fib (" << n << "): " << res2 << std::endl;
 
     //ctxt.calculate_checkpoint();
-    ctxt.calculate_checkpoint();
+    //ctxt.calculate_checkpoint();
 
-    ctxt.print_checkpoint();
+    //ctxt.print_checkpoint();
+
+    timestamp_t t1 = get_timestamp();
+
+    double secs = (t1 - t0) / 1000000.0L;
+    std::cout << "RUNTIME " << secs << std::endl;
 
     return 0;
 }
